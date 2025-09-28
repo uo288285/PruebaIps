5d2cf52bc147728e81b72a4b44cc771845aaf8f1
uo288285 <uo288285@uniovi.es>
2025-09-28 12:35:43 +0200

    Primer commit

---
+++ PruebaIps/src/paquete1/Clase1.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class Clase1 {
+	
+	private String clase1;
+
+	public String getClase1() {
+		return clase1;
+	}
+
+	public void setClase1(String clase1) {
+		this.clase1 = clase1;
+	}
+}
