5f989210fe77640900c6486c2e309e38915e8729
uo288285 <uo288285@uniovi.es>
2025-09-28 12:43:40 +0200

    Segundo commit

---
+++ PruebaIps/src/paquete1/Clase2.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class Clase2 {
+	
+	private int clase2;
+
+	public int getClase2() {
+		return clase2;
+	}
+
+	public void setClase2(int clase2) {
+		this.clase2 = clase2;
+	}
+}
